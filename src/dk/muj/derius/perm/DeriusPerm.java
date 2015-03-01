f344ba63e39b8deca1290a4cc0da0d999841085b
Magnus Ulf <magnus@puj.dk>
2015-03-01 17:50:00 +0100

    Add stamina & sp slot bonus.

--- src/dk/muj/derius/perm/DeriusPerm.java
+++ src/dk/muj/derius/perm/DeriusPerm.java
@@ -3,6 +3,7 @@ package dk.muj.derius.perm;
 import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.permissions.Permission;
@@ -11,11 +12,14 @@ import com.massivecraft.massivecore.MassivePlugin;
 import com.massivecraft.massivecore.util.PermUtil;
 
 import dk.muj.derius.api.Ability;
+import dk.muj.derius.api.DPlayer;
 import dk.muj.derius.api.DeriusAPI;
 import dk.muj.derius.api.Req;
 import dk.muj.derius.api.Skill;
 import dk.muj.derius.api.events.AbilityRegisteredEvent;
 import dk.muj.derius.api.events.SkillRegisteredEvent;
+import dk.muj.derius.api.events.SpecialisationSlotEvent;
+import dk.muj.derius.api.events.StaminaMaxEvent;
 import dk.muj.derius.perm.entity.Perm;
 import dk.muj.derius.perm.entity.PermColl;
 import dk.muj.derius.req.ReqHasPerm;
@@ -162,6 +166,46 @@ public class DeriusPerm extends MassivePlugin
 		Bukkit.getPluginManager().addPermission(learnPermission);
 		Req reqHasLearnPerm = new ReqHasPerm(seePermission);
 		ability.addActivateRequirements(reqHasLearnPerm);
+	}
+	
+	// -------------------------------------------- //
+	// STAMINA
+	// -------------------------------------------- //
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void calcBonusStamina(StaminaMaxEvent event)
+	{
+		DPlayer dplayer = event.getDPlayer();
+		CommandSender sender = dplayer.getSender();
+		if (sender == null) return;
+		
+		for (int i = 300; i > -300; i--)
+		{
+			String node = "derius.stamina.bonus." + String.valueOf(i);
+			if ( ! sender.hasPermission(node)) continue;
+			dplayer.getStaminaBonus().put("perm", new Double(i));
+			break;
+		}
+	}
+	
+	// -------------------------------------------- //
+	// SPECIALISATION SLOTS
+	// -------------------------------------------- //
+	
+	@EventHandler(priority = EventPriority.LOW)
+	public void calcBonusSlots(SpecialisationSlotEvent event)
+	{
+		DPlayer dplayer = event.getDPlayer();
+		CommandSender sender = dplayer.getSender();
+		if (sender == null) return;
+		
+		for (int i = 50; i > -50; i--)
+		{
+			String node = "derius.sp.bonus." + String.valueOf(i);
+			if ( ! sender.hasPermission(node)) continue;
+			dplayer.getSpecialisationSlotBonus().put("perm", i);
+			break;
+		}
 	}
 	
 }
