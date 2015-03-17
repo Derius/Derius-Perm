e3216bd2586eccb799493842249bf4099704fd3a
Magnus Ulf <magnus@puj.dk>
2015-03-17 18:16:23 +0100

    MStore revert

--- src/dk/muj/derius/perm/Const.java
+++ src/dk/muj/derius/perm/Const.java
@@ -6,7 +6,7 @@ public final class Const
 	// BASENAME
 	// -------------------------------------------- //
 	
-	public static final String BASENAME = "derius_perm";
+	public static final String BASENAME = "Derius_Perm";
 	public static final String BASENAME_ = BASENAME + "_";
 	
 	public static final String COLLECTION_PERMS = BASENAME_ + "perms";
